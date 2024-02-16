701e2173bef7d0a03bea2c55f554820fc1e421fd
Leof22 <le322577@ucf.edu>
2024-02-15 20:50:45 -0500

    My First Commit

---
+++ lab_assignment_4.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	char letter;
+	struct node* next;
+} node;
+
+// Returns number of nodes in the linkedList.
+int length(node* head)
+{
+    int length = 0;
+    node* newNode = head;
+
+    while(newNode != NULL)
+    {
+        newNode = newNode -> next;
+    }
+    return length;
+}
+
+// parses the string in the linkedList
+//  if the linked list is head -> |a|->|b|->|c|
+//  then toCString function wil return "abc"
+char* toCString(node* head)
+{
+
+}
+
+// inserts character to the linkedlist
+// f the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as foolows:
+// head -> |a|->|b|->|c|->|x|
+void insertChar(node** pHead, char c)
+{
+}
+
+// deletes all nodes in the linkedList.
+void deleteList(node** pHead)
+{
+  node* newNode = *pHead;
+  while(newNode != NULL)
+    {
+      newNode = newNode -> next;
+      free(newNode);
+      *pHead = newNode;
+    }
+    pHead = NULL;
+}
+
+int main(void)
+{
+	int i, strLen, numInputs;
+	node* head = NULL;
+	char* str;
+	char c;
+	FILE* inFile = fopen("input.txt","r");
+
+	fscanf(inFile, " %d\n", &numInputs);
+
+	while (numInputs-- > 0)
+	{
+		fscanf(inFile, " %d\n", &strLen);
+		for (i = 0; i < strLen; i++)
+		{
+			fscanf(inFile," %c", &c);
+			insertChar(&head, c);
+		}
+
+		str = toCString(head);
+		printf("String is : %s\n", str);
+
+		free(str);
+		deleteList(&head);
+
+		if (head != NULL)
+		{
+			printf("deleteList is not correct!");
+			break;
+		}
+	}
+
+	fclose(inFile);
+}
